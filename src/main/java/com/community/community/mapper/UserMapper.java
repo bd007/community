782aebf3f20b1d47c07ac6735fc514bdedbb6d33
bd007 <769201584@qq.com>
2020-02-13 00:39:25 +0800

    login

--- src/main/java/com/community/community/mapper/UserMapper.java
+++ src/main/java/com/community/community/mapper/UserMapper.java
@@ -4,6 +4,7 @@ package com.community.community.mapper;
 import com.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,4 +12,7 @@ import org.springframework.stereotype.Component;
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}" )
+    User findByToken(String token);
 }
